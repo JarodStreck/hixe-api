48220f06358bfcb4d12ef05d8b8a8bba3ab8c3bf
Jarod <jarod.streckeisen@cpnv.ch>
2020-02-07 09:22:33 +0100

    gitflow-hotfix-stash: model-user-name

---
+++ migrations/20191222075510-create-races.js
@@ -0,0 +1,74 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("races", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      startDate: {
+        type: Sequelize.DATE
+      },
+      endDate: {
+        type: Sequelize.DATE
+      },
+      formType: {
+        type: Sequelize.TEXT
+      },
+      meetingHour: {
+        type: Sequelize.DATE
+      },
+      meetingLocation: {
+        type: Sequelize.STRING
+      },
+      heightDifference: {
+        type: Sequelize.INTEGER
+      },
+      maxParticipant: {
+        type: Sequelize.INTEGER
+      },
+      stateId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "states",
+          key: "id"
+        },
+        defaultValue: 1
+      },
+      difficultyId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "difficulties",
+          key: "id"
+        }
+      },
+      creatorId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "users",
+          key: "id"
+        },
+        defaultValue: 1
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("races");
+  }
+};
---
+++ models/races.js
@@ -0,0 +1,32 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  const races = sequelize.define(
+    "races",
+    {
+      Id: DataTypes.INTEGER,
+      name: DataTypes.STRING,
+      description: DataTypes.TEXT,
+      startDate: DataTypes.DATE,
+      endDate: DataTypes.DATE,
+      formType: DataTypes.TEXT,
+      meetingHour: DataTypes.DATE,
+      meetingLocation: DataTypes.STRING,
+      heightDifference: DataTypes.INTEGER,
+      maxParticipant: DataTypes.INTEGER,
+      creatorId: DataTypes.INTEGER
+    },
+    {}
+  );
+  races.associate = function (models) {
+    races.belongsTo(models.state);
+    races.belongsTo(models.difficulties);
+    races.belongsToMany(models.materials, {
+      through: models.neededMaterials
+    });
+    races.belongsToMany(models.users, {
+      through: models.participants
+    });
+    races.belongsTo(models.users, { as: "Creator" });
+  };
+  return races;
+};
